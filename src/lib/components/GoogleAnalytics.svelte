7b434cfeed30ef552b27ed5fe2705c6bc55a0748
Omar Al Matar <bewinxed@gmail.com>
2023-10-08 06:34:00 +0200

    added ganalytics

---
+++ src/lib/components/GoogleAnalytics.svelte
@@ -0,0 +1,33 @@
+<script lang="ts">
+	import { page } from '$app/stores';
+
+    
+	$: {
+        // @ts-expect-error ignore this, ganalytics
+		if (typeof gtag !== 'undefined') {
+            // @ts-expect-error ignore this, ganalytics
+			gtag('config', '914DZTMXZC', {
+				page_title: document.title,
+				page_path: $page.url.pathname
+			});
+		}
+	}
+</script>
+
+<svelte:head>
+	<script
+		async
+		src="https://www.googletagmanager.com/gtag/js?id=MEASUREMENT_ID"
+	>
+	</script>
+	<script>
+		window.dataLayer = window.dataLayer || [];
+
+		function gtag() {
+			dataLayer.push(arguments);
+		}
+
+		gtag('js', new Date());
+		gtag('config', 'MEASUREMENT_ID');
+	</script>
+</svelte:head>
