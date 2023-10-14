e914b59423ec0754d9b5c04fc69e54af38f2257b
Omar Al Matar <bewinxed@gmail.com>
2023-10-14 23:34:27 +0200

    Added CORS middleware

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -4,6 +4,18 @@ import { error, redirect, type Handle } from '@sveltejs/kit';
 export const handle: Handle = async ({ event, resolve }) => {
 	event.locals.auth = auth.handleRequest(event);
 	// console.log(event.url.pathname)
+
+	// Apply CORS header for API routes
+	if (event.url.pathname.startsWith('/api') && event.request.method === 'OPTIONS') {
+       return new Response(null, {
+ 			headers: {
+ 			  'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, PATCH, OPTIONS',
+ 			  'Access-Control-Allow-Origin': '*',
+ 			  'Access-Control-Allow-Headers': '*',
+ 			}
+ 		  });
+ }
+
 	if (event.url.pathname.startsWith('/api/user')) {
 		const session = await event.locals.auth.validate();
 		if (!session) {
@@ -20,5 +32,11 @@ export const handle: Handle = async ({ event, resolve }) => {
 		}
 	}
 
-	return await resolve(event);
+	const response = await resolve(event);
+  if (event.url.pathname.startsWith('/api')) {
+    response.headers.append('Access-Control-Allow-Origin', `*`);
+  }
+
+	return response;
 };
+
