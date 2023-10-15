4938230bd080ca5fee860251575c144ae9ac256d
Omar Al Matar <bewinxed@gmail.com>
2023-10-15 21:07:01 +0200

    add polyfills, fix redirects

--- src/lib/server/lucia.ts
+++ src/lib/server/lucia.ts
@@ -34,8 +34,8 @@ export const auth = lucia({
 
 
 export const solana_oauth = solanaAuth(auth, {
-    uri: "http://127.0.0.1:5173/auth",
-    validationUri: "http://127.0.0.1:5173/api/auth/session",
+    uri: dev ? "http://127.0.0.1:5173/auth" : "https://deauth.xyz/api/auth",
+    validationUri: dev ? "http://127.0.0.1:5173/api/auth/session" : "https://deauth.xyz/api/auth/session",
     redirectUri: dev ? "http://127.0.0.1:5173/api/auth/callback/solana" : "https://deauth.xyz/api/auth/callback/solana",
     scope: ["scope1", "scope2"],
     rpc: "https://api.mainnet-beta.solana.com/",
--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,17 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vitest/config';
+import { nodePolyfills } from 'vite-plugin-node-polyfills'
+
 
 export default defineConfig({
-	plugins: [sveltekit()],
+	plugins: [sveltekit(), nodePolyfills({
+		globals: {
+			Buffer: true,
+			process: true,
+			global: true,
+		},
+		protocolImports: true,
+	})],
 	test: {
 		include: ['src/**/*.{test,spec}.{js,ts}']
 	},
